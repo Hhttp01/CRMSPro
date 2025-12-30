e79bcc1b2db56ca0aee2285970bd69c7bfd459a5
Hhttp01 <osismadar16@gmail.com>
2025-12-30 14:42:12 +0200

    Create לקוח.js

---
+++ server/models/לקוח.js
@@ -0,0 +1,6 @@
+// כאן נשתמש במערך פשוט במקום מסד נתונים כדי שהמערכת תעבוד מיידית
+let customers = [
+    { id: 1, name: "ישראל ישראלי", email: "israel@test.com", phone: "050-1234567" }
+];
+
+module.exports = customers;

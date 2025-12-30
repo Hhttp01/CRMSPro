33df052784b1092f007127aef9ec1cd2e7a8e949
Hhttp01 <osismadar16@gmail.com>
2025-12-30 15:44:53 +0200

    Update לקוח.js

--- server/models/לקוח.js
+++ server/models/לקוח.js
@@ -1,6 +1,15 @@
-// כאן נשתמש במערך פשוט במקום מסד נתונים כדי שהמערכת תעבוד מיידית
+class Customer {
+    constructor(id, name, email, phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
+
+// מערך לאחסון זמני
 let customers = [
     { id: 1, name: "ישראל ישראלי", email: "israel@test.com", phone: "050-1234567" }
 ];
 
-module.exports = customers;
+module.exports = { Customer, customers };
